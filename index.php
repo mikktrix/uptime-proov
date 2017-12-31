64dc1af03e897940ccdbcaffb247c3f5d7a63356
mikktrix <34936671+mikktrix@users.noreply.github.com>
2017-12-31 13:44:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<script type="text/javascript" src="jquery-3.2.1.min.js"></script>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width" />
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title>Block Proov</title>
 </head>
